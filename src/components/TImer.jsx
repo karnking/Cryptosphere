f065231414559a90ce83f37bded5c78596680a57
karnking <karankotai9183@gmail.com>
2023-10-27 18:13:00 +0530

    new component

---
+++ src/components/TImer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const TImer = () => {
+  return (
+    <div>
+        This will be a timer import React from 'react'
+        
+        export const Component = (props) 
+            return(
+                <div>
+                    {props.children}
+                </div>
+            )
+        
+        
+        export default Component
+    </div>
+  )
+}
+
+export default TImer
